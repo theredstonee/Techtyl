c0a158a51ad80cddd156c58b2073296bf039a367
theredstonee <blitz.der.wahrheit@gmail.com>
2025-11-09 11:33:19 +0100

    refactor: Remove unnecessary copyright and licensing comments from install script

--- techtyl-installer/install.sh
+++ techtyl-installer/install.sh
@@ -2,31 +2,7 @@
 
 set -e
 
-######################################################################################
-#                                                                                    #
-# Project 'techtyl-installer'                                                        #
-# Based on 'pterodactyl-installer'                                                   #
-#                                                                                    #
-# Original Copyright (C) 2018 - 2025, Vilhelm Prytz, <vilhelm@prytznet.se>          #
-# Techtyl Adaptation (C) 2025                                                        #
-#                                                                                    #
-#   This program is free software: you can redistribute it and/or modify             #
-#   it under the terms of the GNU General Public License as published by             #
-#   the Free Software Foundation, either version 3 of the License, or                #
-#   (at your option) any later version.                                              #
-#                                                                                    #
-#   This program is distributed in the hope that it will be useful,                  #
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of                   #
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    #
-#   GNU General Public License for more details.                                     #
-#                                                                                    #
-#   You should have received a copy of the GNU General Public License                #
-#   along with this program.  If not, see <https://www.gnu.org/licenses/>.           #
-#                                                                                    #
-# This script is adapted for Techtyl and is not associated with the                 #
-# official Pterodactyl Project.                                                      #
-#                                                                                    #
-######################################################################################
+
 
 export GITHUB_SOURCE="main"
 export SCRIPT_RELEASE="v1.3.0"
